5e53fb6ab1c7c12b02d85ac42cfb09885e451c8a
uru <muru@mail.ru>
2021-08-19 12:07:58 +0300

    Initial Commit

---
+++ philo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  philo
+//
+//  Created by Mikhail Uryvaev on 19.08.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
