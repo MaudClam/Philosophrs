50d668b900a41e575159e3718ac9f63f0357bd6d
uru <muru@mail.ru>
2021-09-01 01:06:32 +0300

    utils2 changed

--- philo/utils2.c
+++ philo/utils2.c
@@ -38,54 +38,27 @@ void	ft_swap(long *a, long *b)
 	*a = *a ^ *b;
 }
 
-/*
-** start of ft_atoi()
-*/
-
-static int	islonglong(unsigned long nbr, int sign)
-{
-	if (nbr >= LLONG_MAX && sign > 0)
-		return (-1);
-	else if (nbr > LLONG_MAX)
-		return (0);
-	return ((int)nbr);
-}
-
-static int	ft_isspace(char c)
-{
-	if (c == '\t' || c == '\n' || c == '\r' \
-					|| c == '\v' || c == '\f' || c == ' ')
-		return (1);
-	return (0);
-}
-
 int	ft_atoi(const char *str)
 {
+	char				sign;
 	unsigned long int	nbr;
-	int					sign;
-	int					i;
 
 	nbr = 0;
 	sign = 1;
-	if (!str || !str[0])
-		return (0);
-	i = 0;
-	while (str[i] && ft_isspace(str[i]))
-		i++;
-	if (str[i] == '-')
-	{
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+			str++;
+	if (*str == '-')
 		sign = -1;
-		i++;
-	}
-	else if (str[i] == '+')
-		i++;
-	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	if (*str == '+' || *str == '-')
+		str++;
+	while (*str && *str >= '0' && *str <= '9')
 	{
-		nbr = nbr * 10 + (str[i] - '0');
-		i++;
+		nbr = nbr * 10 + (*str - '0');
+		if (nbr >= LLONG_MAX && sign > 1)
+			return (-1);
+		else if (nbr > LLONG_MAX)
+			return (0);
+		str++;
 	}
-	return (islonglong(nbr, sign) * sign);
+	return (sign * (int)nbr);
 }
-/*
-** end of ft_atoi()
-*/
